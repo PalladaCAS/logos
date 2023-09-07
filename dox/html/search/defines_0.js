bbdde56c5e27bca538ac026b2b56d6920495a248
Centrix14 <artiom.zvyagin@yandex.ru>
2023-09-07 18:40:26 +0300

    Remove documentation.

--- dox/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['abs_17',['ABS',['../define_8h.html#a996f7be338ccb40d1a2a5abc1ad61759',1,'define.h']]]
-];
--- dox/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['test_11',['test',['../logos_8h.html#a91268191972aa1024b8579cb275c19a4',1,'logos.h']]]
-];
